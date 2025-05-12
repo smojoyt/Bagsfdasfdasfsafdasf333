3b06ceb56ed0281cf7e7c191de869c84b1e3f2e9
justinlmcneal <mcneal.justin99@gmail.com>
2025-05-12 19:44:37 -0400

    ees

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,34 +1,47 @@
 ﻿const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+const products = {
+    "Bag_MiniHeart": {
+        name: "Mini Tote - Heart Embossed",
+        price: 1999
+    },
+    // Add more products here
+};
+
 module.exports = async (req, res) => {
-    if (req.method === 'POST') {
+    if (req.method === "POST") {
         try {
+            const { sku } = req.body;
+            const product = products[sku];
+
+            if (!product) {
+                return res.status(400).json({ error: "Invalid SKU" });
+            }
+
             const session = await stripe.checkout.sessions.create({
                 payment_method_types: ['card'],
-                line_items: [
-                    {
-                        price_data: {
-                            currency: 'usd',
-                            product_data: {
-                                name: 'Mini Tote - Heart Embossed',
-                            },
-                            unit_amount: 1999,
+                line_items: [{
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: product.name,
                         },
-                        quantity: 1,
+                        unit_amount: product.price
                     },
-                ],
+                    quantity: 1
+                }],
                 mode: 'payment',
-                success_url: `${req.headers.origin}/`,
-                cancel_url: `${req.headers.origin}/`,
+                success_url: `https://www.karrykraze.com/`,
+                cancel_url: `https://www.karrykraze.com/`,
             });
 
             res.status(200).json({ url: session.url });
         } catch (err) {
-            console.error("Stripe Error:", err.message);
+            console.error("Stripe error:", err.message);
             res.status(500).json({ error: err.message });
         }
     } else {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
+        res.setHeader("Allow", "POST");
+        res.status(405).end("Method Not Allowed");
     }
 };
