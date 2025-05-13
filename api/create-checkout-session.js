f09ab7d8323bd310c2662d889e0c8f41ff655f34
justinlmcneal <mcneal.justin99@gmail.com>
2025-05-13 01:36:56 -0400

    d

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -96,7 +96,7 @@ export default async function handler(req, res) {
         }
 
         const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
+            payment_method_types: ['card', 'link', 'klarna', 'cashapp', 'affirm', 'afterpay_clearpay'],
             mode: 'payment',
             line_items,
             customer_creation: 'always',
