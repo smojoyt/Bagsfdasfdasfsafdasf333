8a27b689ac43ebe9d4d7a50dfc8283a6fb276d69
justinlmcneal <mcneal.justin99@gmail.com>
2025-07-08 23:12:17 -0400

    dwa

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Third-party libraries (non-modular, loaded via script tags)
+import 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js';
+import 'https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js';
+
+// Your custom scripts
+import './helpers.js';
+import './cartUtils.js';
+import './bundleEngine.js';
+import './cartRenderer.js';
+import './checkout.js';
+import './uni_navbar.js';
+import './moreItemsCarousel.js';
+import './loadProductData.js';
