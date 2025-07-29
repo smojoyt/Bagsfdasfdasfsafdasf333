02143ba05c76fb1a3b325db977b47e680cd5c48d
justinlmcneal <mcneal.justin99@gmail.com>
2025-07-28 23:19:49 -0400

    w

--- js/Cards/Catalog/catalogCard.js
+++ js/Cards/Catalog/catalogCard.js
@@ -32,7 +32,7 @@ export async function createCatalogCard(sku, product) {
   <!-- Like Button Over Image -->
 <!-- Like Button -->
 <div class="like-wrapper absolute bottom-2 left-2 z-10 flex items-center gap-1 px-2 py-1 bg-white rounded-full shadow-sm" data-sku="${sku}" data-productid="${product.productId || sku}">
-<button class="like-btn flex items-center justify-center w-4 h-4 md:w-7 md:h-7 rounded-full transition-all" aria-label="Like">
+<button class="like-btn flex items-center justify-center w-3 h-3 md:w-7 md:h-7 rounded-full transition-all" aria-label="Like">
 <svg class="w-5 h-5" viewBox="-1.6 -1.6 19.20 19.20" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="1.5">
   <path
     d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z"
